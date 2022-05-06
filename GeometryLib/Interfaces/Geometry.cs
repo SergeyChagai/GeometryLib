532368282dcc6895ef677d6edff96f45c4023132
Chagaj Sergey <chagaj42@gmail.com>
2022-05-06 11:00:16 +0400

    Initial commit

---
+++ GeometryLib/Interfaces/Geometry.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeometryLib.Interfaces
+{
+    public interface Geometry
+    {
+        public double Square();
+    }
+}
